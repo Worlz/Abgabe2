6bf9b8e57d23b0ece95b3c6be9c27b56e936116c
Worlz <scwmart@googlemail.com>
2017-07-02 22:56:29 +0200

    Final
    Alle Features sind drin außer Sound, Animationen und Gegner

---
+++ Abgabe2/Assets/Script/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	//public GameObject Small_Enemy;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update()
+	{
+		if (restart == true) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene("GameScene");
+
+			}
+		}
+	}
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+			
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, Random.Range (-spawnValues.z, spawnValues.z));
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver == true) 
+			{
+				restartText.text = "Drücke 'R' für Neustarten";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+		void UpdateScore()
+		{
+			scoreText.text = "Score: " + score;
+		}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+
+	}
--- Abgabe2/Assets/Script/PlayerController.cs
+++ Abgabe2/Assets/Script/PlayerController.cs
@@ -12,8 +12,23 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 	public Boundary boundary;
-	
 	private Rigidbody rb;
+
+	public GameObject shot;
+	public GameObject shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update()
+	{
+		if(Input.GetButton("Fire1") && Time.time > nextFire)
+			{
+				nextFire = Time.time + fireRate;
+				Instantiate(shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
+			}
+	}
+
 	void FixedUpdate()
 	{
 		
